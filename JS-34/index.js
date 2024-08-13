30e7483edf755899aa5d29cdcb90b173de030aae
kineticbeast69 <139427930+kineticbeast69@users.noreply.github.com>
2024-08-13 21:17:45 +0530

    MS word Office Features

---
+++ JS-34/index.js
@@ -0,0 +1,57 @@
+const paraText = document.getElementById("para");
+const icons = document.querySelectorAll(".icon");
+const sizeIcons = document.querySelectorAll(".size-icon");
+const weightIcons = document.querySelectorAll(".weight-icon");
+icons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    const value = icon.value;
+    const paraValue = paraText.value;
+    if (value === "Uppercase") {
+      paraText.value = paraValue.toUpperCase();
+    } else if (value === "Lowercase") {
+      paraText.value = paraValue.toLowerCase();
+    } else if (value === "Capitalize") {
+      paraText.value = paraValue
+        .split(" ")
+        .map((word) => {
+          return word.charAt(0).toUpperCase() + word.slice(1);
+        })
+        .join(" ");
+    } else if (value === "Toggle") {
+      paraText.value = paraValue
+        .split(" ")
+        .map((word) => {
+          return word.charAt(0).toLowerCase() + word.slice(1).toUpperCase();
+        })
+        .join(" ");
+    } else {
+      paraText.value = paraValue;
+    }
+  });
+});
+
+let count = 1;
+sizeIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let value = icon.value;
+    if (value === "A+") {
+      count += 0.1;
+    } else {
+      count -= 0.1;
+    }
+    paraText.style.fontSize = `${count}em`;
+  });
+});
+
+weightIcons.forEach((icon) => {
+  icon.addEventListener("click", () => {
+    let value = icon.value;
+    if (value === "Bold") {
+      paraText.style.fontWeight = "bold";
+    } else if (value === "Italic") {
+      paraText.style.fontStyle = "italic";
+    } else if (value === "underline") {
+      paraText.style.textDecoration = "underline";
+    }
+  });
+});
